e965f98146bb1a80c2bdc0530400d79796d77249
LiptonZIntru <vili2002@centrum.cz>
2021-03-24 21:27:16 +0100

    Initial commit

---
+++ RockPaperScissorsClient/RequestProviders/RequestProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RockPaperScissorsClient.RequestProviders
+{
+    class RequestProvider
+    {
+        static RequestProvider requestProvider;
+
+        public static RequestProvider Create()
+        {
+            requestProvider = new RequestProvider();
+            return requestProvider;
+        }
+        public async void Run()
+        {
+
+        }
+    }
+}
